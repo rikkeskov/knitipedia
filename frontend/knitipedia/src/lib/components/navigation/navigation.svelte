2dd713ad39398293cb332d6800fc3c1fc375b05e
Rasmus Møller <rmn@sandbox.ventures>
2025-08-03 09:02:17 +0200

    Added shadcn-svelte

---
+++ frontend/knitipedia/src/lib/components/navigation/navigation.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let links = [
+		{ name: 'Home', href: '/' },
+		{ name: 'Patterns', href: '/patterns' },
+		{ name: 'Yarns', href: '/yarns' },
+		{ name: 'About', href: '/about' }
+	];
+</script>
+
+<nav class="border-b border-gray-200 bg-gray-50 p-4">
+	<ul class="m-0 flex list-none gap-6 p-0">
+		{#each links as link}
+			<li>
+				<a
+					href={link.href}
+					class="font-medium text-gray-800 no-underline transition-colors duration-200 hover:text-blue-600"
+				>
+					{link.name}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
--- frontend/knitipedia/src/routes/Header.svelte
+++ frontend/knitipedia/src/routes/Header.svelte
@@ -12,9 +12,6 @@
 	</div>
 
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
 		<ul>
 			<li aria-current={page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
@@ -26,41 +23,15 @@
 				<a href="/sverdle">Sverdle</a>
 			</li>
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
 	</nav>
 
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
+	<div class="corner"></div>
 </header>
 
 <style>
 	header {
 		display: flex;
 		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
 	}
 
 	nav {
